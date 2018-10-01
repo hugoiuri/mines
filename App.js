536d090c1fbbe451cbc9d15cf80a78cabe199ccf
hugoiuri <hugoraiuri@gmail.com>
2018-09-30 21:05:17 -0300

    logica de construção de tabuleiro

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {Component} from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 import params from './src/params'
 import Field from './src/components/Fields'
 
